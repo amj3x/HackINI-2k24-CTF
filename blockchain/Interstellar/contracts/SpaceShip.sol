65bf4bbc75b3a708684353532434ceb5c4fbac87
Amdjed Zerrougui <102095228+0x0000000000000j3x@users.noreply.github.com>
2023-12-13 10:58:34 +0000

    Update SpaceShip.sol

--- blockchain/Interstellar/contracts/SpaceShip.sol
+++ blockchain/Interstellar/contracts/SpaceShip.sol
@@ -4,20 +4,27 @@ pragma solidity ^0.7.0;
 contract SpaceShip {
     uint32 public distanceFromTheBlackHole;
     uint32 private distanceTraveled;
+    address public controlSystem;
 
     constructor() {
         distanceTraveled = 1337;
         distanceFromTheBlackHole = 4294967295;
+        controlSystem = msg.sender;
     }
 
-    function galacticBoost(uint32 advanceWithDistance) public {
+    function galacticBoost(uint32 advanceWithDistance) public onlyControlSystem {
         // Avoid the space ship from falling into the black hole
         require(distanceTraveled + advanceWithDistance < distanceFromTheBlackHole);
         distanceTraveled += advanceWithDistance;
     }
 
+    
     function getDistanceTraveled() public view returns (uint256) {
         return distanceTraveled;
+    }
+
+    modifier onlyControlSystem() {
+            require(tx.origin == controlSystem);
     }
 }
 
